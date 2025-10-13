60871ca2e3d940ff7fdfe7e383359d69b024272c
Паркулаб Василь <slay9817@gmail.com>
2025-10-13 09:36:34 +0100

    Updates

--- config-overrides.js
+++
@@ -1,8 +0,0 @@
-// config-overrides.js
-module.exports = function override(config) {
-    if (config.optimization) {
-        config.optimization.minimize = false // stop minifying JS
-    }
-    config.devtool = 'source-map' // keep full source maps
-    return config
-}
